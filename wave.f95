e764af1b4711e2bac04ed627d9172a2b150e56e7
Atila <atilasaraiva@gmail.com>
2019-09-04 17:11:37 -0300

    Commenting

--- wave.f95
+++ wave.f95
@@ -1,58 +1,88 @@
 module difFinitas
+    !=============================================!
+    ! Módulo desenvolvido para efetuar a modelagem
+    ! direta da propagação de uma onda ao resolver
+    ! a equação da onda acústica num campo de vel-
+    ! ocidade específico.
+    !
+    ! Autor: Átila Saraiva Quintela Soares
+    ! Email: atilasaraiva@gmail.com
+    !
+    ! Contribuição: Protásio
+    !=============================================!
     implicit none
     integer,parameter:: dp = kind(0.d0)
     real,parameter:: numPi = 3.14159263
     contains
 
-        subroutine waveEstrap (ordem,Nz,Nx,Nt,dx,dz,dt,wavelet,campoVel,fontes)
-            integer,intent(in) :: Nx,Nz,Nt,fontes(:,:),ordem
+        subroutine waveEstrap (ordem,Nz,Nx,Nt,Nb,dx,dz,dt,wavelet,campoVel,fontes)
+            !=============================================!
+            ! Subrotina para fazer a propagação de uma
+            ! onda num campo de velocidade.
+            !=============================================!
+
+            ! Entrada e Saída
+            integer,intent(in) :: Nx,Nz,Nt,fontes(:,:),ordem,Nb
             real,intent(in)    :: campoVel(Nz,Nx),dx,dt,dz,wavelet(:)
-            real,allocatable   :: P_futuro(:,:),P_passado(:,:),P_atual(:,:)
-            real,allocatable   :: lap(:,:)
-            real,allocatable   :: campoVelExt(:,:)
-            real               :: coefAtenuacao(Nb)
-            character(len=30)  :: filename
-            real               :: waveletExtendida(Nt),prod=1.0
-            integer            :: i,j,k,L,f1,f2,j1,lFontes,counter,Nb,Nxb,Nzb
 
+            ! Variáveis Auxiliares
+            real,allocatable   :: P_futuro(:,:),P_passado(:,:),P_atual(:,:) ! Campos de pressão
+            real,allocatable   :: lap(:,:)                                  ! Laplaciano
+            real,allocatable   :: campoVelExt(:,:)                          ! Campo de velocidade com borda de atenuacao
+            real               :: coefAtenuacao(Nb)                         ! Coefientes de atenuacao
+            character(len=30)  :: filename                                  ! Nome do Arquivo
+            real               :: prod=1.0                                  ! Aux de produto
+            integer            :: i,j,k,&                                   ! Contadores
+                                  f1,f2,lFontes,&                           ! Relacionados a posição e num de fontes
+                                  Nxb,Nzb                                   ! Dimensões com a borda
+
+            ! Criando arquivo para guardar os snaps
             filename = 'snap.ad'
-
             open(30,file=filename,status='replace',recl=4*Nz*Nx,form='unformatted',access='direct')
 
             ! Extraindo a quantidade de fontes
             lFontes = ubound(fontes,2)
 
 
-            Nb = 30
+            ! Alocando arrays
             Nxb = Nx + 2*Nb
             Nzb = Nz + 2*Nb
             allocate(campoVelExt(Nzb,Nxb))
             allocate(P_futuro(Nzb,Nxb),P_passado(Nzb,Nxb),P_atual(Nzb,Nxb),lap(Nzb,Nxb))
 
+            ! Extendendo o campo de velocidade com a borda de atenuação
             campoVelExt = extenCampoVel(Nx,Nz,Nb,campoVel)
 
+            ! Zerando os campos
             P_passado = 0.0
             P_atual = 0.0
             P_futuro = 0.0
 
+            ! Calculando os coeficientes de Atenuação para serem multiplicados na Borda
             coefAtenuacao = coeficientesDeAtenuacao(Nb)
 
             j=1
-            !do k=2,Nt-1
-            do k=2,1000
+            do k=2,Nt-1
                 write(0,*) 'it',k
-                call atenuacao(nxb,nzb,nb,P_passado)
-                call atenuacao(nxb,nzb,nb,P_atual)
 
+                ! Atenuando os campos passado e presente
+                call atenuacao(nxb,nzb,nb,coefAtenuacao,P_passado)
+                call atenuacao(nxb,nzb,nb,coefAtenuacao,P_atual)
+
+                ! Calculando o laplaciano
                 lap = laplaciano(Nb,ordem,Nxb,Nzb,dx,dz,P_atual)
+
+                ! Resolvendo a equação da onda por diferenças finitas
                 P_futuro = (2.0*P_atual - P_passado  + (dt**2.)*(campoVelExt**2.)*lap)! * mascaraAtenuacaoBorda
 
+                ! Inserindo a wavelet nas posições de fonte
                 do i=1,lFontes
                     f1=fontes(1,i)+Nb
                     f2=fontes(2,i)+Nb
                     P_futuro(f1,f2) = wavelet(k) + P_futuro(f1,f2)
                 end do
 
+                ! Se a iteração for múltiplo de 20, escrever o campo no arquivo
                 if(mod(k,20) == 0)then
                     write(30,rec=j) P_futuro(Nb+1:Nz+Nb,Nb+1:Nx+Nb)
                     j = j + 1
@@ -65,78 +95,73 @@ module difFinitas
         end subroutine waveEstrap
 
         function coeficientesDeAtenuacao(Nb)
+            !=============================================!
+            ! Função para calcular os coeficientes de
+            ! atenuação da onda na borda.
+            !=============================================!
+
             ! Entrada e Saída
             integer,intent(in):: Nb
-            real:: coefAtenuacao(Nb)
+            real:: coeficientesDeAtenuacao(Nb)
             ! Auxiliares
             integer:: i
 
+            ! Calculando os coeficientes
             do i =1,nb
-               coefAtenuacao(i) = exp(-(0.008*(nb-i))**2)
+               coeficientesDeAtenuacao(i) = exp(-(0.008*(nb-i))**2)
             enddo
+
+            return
         end function coeficientesDeAtenuacao
 
-
         subroutine atenuacao(nxb,nzb,nb,coef,p2)
-            integer:: nxb, nzb,nb,i,j,lz,lx
+            !=============================================!
+            ! Subrotina para a atenuação de borda da onda
+            !=============================================!
+
+            ! Entrada e Saída
+            integer:: nxb, nzb,nb
             real,intent(in):: coef(Nb)
             real, dimension (:,:):: p2(nzb,nxb)
+            ! Auxiliares
+            integer:: i,j,lz,lx
 
             lz=nzb
             lx=nxb
             do i =1,nb
-               do j=1,nxb
-                   p2(i,j)=p2(i,j)*coef(i)
-                   p2(lz,j)=p2(lz,j)*coef(i)
-               end do
-               do j=1,nzb
-                   alpha = exp(-(0.008*(nb-i))**2)
-                   p2(j,i)=p2(j,i)*coef(i)
-                   p2(j,lx)=p2(j,lx)*coef(i)
-               enddo
-               lx=lx-1
-               lz=lz-1
+                do j=1,nxb
+                    p2(i,j)=p2(i,j)*coef(i)
+                    p2(lz,j)=p2(lz,j)*coef(i)
+                end do
+                do j=1,nzb
+                    p2(j,i)=p2(j,i)*coef(i)
+                    p2(j,lx)=p2(j,lx)*coef(i)
+                enddo
+                lx=lx-1
+                lz=lz-1
             enddo
 
             return
         end subroutine atenuacao
 
-
-        function mascaraAtenuacao(Nx,Nz,Nb,dx,dz)
-            integer,intent(in):: Nx,Nz,Nb
-            real,intent(in)::dx,dz
-            real:: mascaraAtenuacao(-Nb+1:Nz+Nb,-Nb+1:Nx+Nb),alpha
-            integer:: i,j
-            real:: x,z
-
-            mascaraAtenuacao(1:Nz,1:Nx) = 1.0
-
-            do j=-Nb+1,0
-                x = j*dx
-                z = j*dz
-                alpha=0.15**(-x/(Nb*dx))
-                mascaraAtenuacao(:,j) = alpha
-                mascaraAtenuacao(:,-j+1+Nx) = alpha
-                alpha=0.15**(-z/(Nb*dz))
-                mascaraAtenuacao(j,1:Nx) = alpha
-                mascaraAtenuacao(-j+1+Nx,1:Nx) = alpha
-            end do
-
-            open(40,file="mascara.ad",status='replace',recl=4*(Nz+2*Nb),form='unformatted',access='direct')
-
-            do j=-Nb+1,Nx+Nb
-                write(40,rec=j+Nb) mascaraAtenuacao(:,j)
-            end do
-            close(40)
-        end function mascaraAtenuacao
-
         function extenCampoVel (Nx,Nz,Nb,campoVel) result(campoVelExt)
+            !=============================================!
+            ! Função para extensão do campo de velocidade
+            ! para que seja possível se criar uma borda de
+            ! atenuação.
+            !=============================================!
+            ! Entrada e Saída
             integer,intent(in):: Nx,Nz,Nb
             real,intent(in):: campoVel(Nz,Nx)
             real:: campoVelExt(-Nb+1:Nz+Nb,-Nb+1:Nx+Nb)
+            ! Auxiliares
             integer:: i,j
 
+            ! Copiando o campo original no centro do extendido
             campoVelExt(1:Nz,1:Nx) = campoVel
+
+            ! Copiando os valores do contorno do campo de vel original
+            ! na borda do campo extendido
             do i=-Nb+1,0
                 campoVelExt(i,1:Nx) = campoVel(1,:)
                 campoVelExt(1:Nz,i) = campoVel(:,1)
@@ -145,6 +170,8 @@ module difFinitas
                 campoVelExt(1:Nz,Nx+j) = campoVel(:,Nz)
             end do
 
+            ! Fazendo o mesmo para os valores nos 4 cantos do campo de
+            ! velocidade original.
             campoVelExt(-Nb+1:0,-Nb+1:0) = campoVel(1,1)
             campoVelExt(-Nb+1:0,Nx+1:Nx+Nb) = campoVel(1,Nx)
             campoVelExt(Nz+1:Nz+Nb,-Nb+1:0) = campoVel(Nz,1)
@@ -153,6 +180,12 @@ module difFinitas
         end function extenCampoVel
 
         function laplaciano(Nb,ordem,Nx,Nz,dx,dz,P)
+            !=============================================!
+            ! Subrotina para cálculo do laplaciano de um
+            ! campo de pressão, com ordens de 2,4,6 e 8.
+            ! !=============================================!
+
+            ! Entrada e Saída
             integer,intent(in):: Nx,Nz,ordem,Nb
             real,intent(in):: dx,dz,P(Nz,Nx)
             real:: laplaciano(Nx,Nz)
@@ -162,6 +195,8 @@ module difFinitas
             integer:: i,j,k,in_n,lim_nx,lim_nz
             laplaciano = 0.0
 
+            ! Calculo dos coeficientes do operador de
+            ! diferenças finitas
             coef = 0.0
             select case (ordem)
 
@@ -195,30 +230,32 @@ module difFinitas
                     coef(9) = -1./560.
                 case default
                     write(0,*) "Ordem inválida"
-
+                    return
             end select
 
+            ! Definindo os limites em que o operador vai trabalhar
             in_n   = ordem / 2 + 1
             lim_nx = Nx - ordem / 2
             lim_nz = Nz - ordem / 2
+
+            ! Calculando o laplaciano
             do j=in_n,lim_nx
                 do i=in_n,lim_nz
                     Pxx = 0.0
                     Pzz = 0.0
 
                     do k=1,ordem+1
+                        ! Derivada em x
                         Pxx = Pxx + coef(k)*P(i,j+k-in_n)
+                        ! Derivada em z
                         Pzz = Pzz + coef(k)*P(i+k-in_n,j)
                     end do
 
-    !                if(j<=Nb .or. j>=Nx-Nb+1 .or. i<=Nb .or. i>=Nz-Nb+1)then
-    !                    laplaciano(i,j) = Pxx / (dx*10.0)**2. + Pzz / ((dz*10.0)**2.)
-    !                else
-                        laplaciano(i,j) = Pxx / dx**2. + Pzz / (dz**2.)
-    !                end if
+                    laplaciano(i,j) = Pxx / dx**2. + Pzz / (dz**2.)
                 end do
             end do
 
+            return
         end function laplaciano
 
 end module difFinitas
@@ -226,30 +263,32 @@ end module difFinitas
 program wave
     use difFinitas
     implicit none
-    integer,parameter:: Nx=200,Nz=200,Nt=1500,ordem=8
+    ! Parâmetros do modelo
+    integer,parameter:: Nx=200,Nz=200,Nt=1500,ordem=8,Nb = 40
     real,parameter:: h1=1000,h2=1000
-    real,parameter:: dx=10.0,dz=10.0
-    real:: dt, campoVel(Nz,Nx),t,fmax,tAux
+    real,parameter:: dx=10.0,dz=10.0,dt=0.001
+    ! Campo de velocidade
+    real:: campoVel(Nz,Nx)
+    ! Wavelet
     real:: wavelet(Nt)
+    ! Auxiliares
     integer:: i,j,h1A,h2A,fontes(2,1),k,counter
 
+    ! Criação do campo de velocidade
     h1A = Nz/3.0
     h2A = h1A
     campoVel(1:h1A,1:Nx) = 2000.0
     campoVel(h1A:h2A+h1A,1:Nx) = 3500.0
     campoVel(h2A+h1A:Nz,1:Nx) = 4000.0
 
+    ! Estabelecimento da posição das fotnes
     fontes(:,1) = [Nz/2,Nx/2]
 
-    dt=0.001
-
+    ! Leitura do pulso
     open(50,file="pulso.ad",status='old',recl=4*Nt,form='unformatted',access='direct')
-    !open(40,file="campovel.ad",status='replace',recl=4*Nz,form='unformatted',access='direct')
-
     read(50,rec=1) wavelet
-
-    call waveEstrap (ordem,Nz,Nx,Nt,dx,dz,dt,wavelet,campoVel,fontes)
     close(50)
-    !close(40)
 
+    ! Chamada da subrotina de propagação da onda
+    call waveEstrap (ordem,Nz,Nx,Nt,Nb,dx,dz,dt,wavelet,campoVel,fontes)
 end program wave
